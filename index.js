ad6ea921f3acd20ea415c78c99ce4311706d1ded
nickubed <nhpquandt@gmail.com>
2019-12-12 14:18:19 -0800

    Added EJS Layouts

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Required node modules
 let express = require('express')
+let layouts = require('express-ejs-layouts')
 
 // Declare express app variable
 let app = express();
 
 // Set up and middleware
-
+app.set('view engine', 'ejs')
+app.use(layouts)
 // Add any controllers
 
 // Add home or catch-all routes
 app.get('/', (req, res) => {
-    res.send('<h1>Stubzilla</h1>')
+    res.render('home')
 })
 
 
