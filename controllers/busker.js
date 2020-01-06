edd606dd07df67393f971d3e2249c7178ea5e47a
nickubed <nhpquandt@gmail.com>
2020-01-06 09:36:03 -0800

    Mapbox functioning, but needs to be moved.

--- controllers/busker.js
+++ controllers/busker.js
@@ -1,12 +1,20 @@
 let router = require('express').Router()
 let db = require('../models')
 let isLoggedIn = require('../middleware/isLoggedIn')
+// let mapboxgl = require('mapbox-gl')
 
 router.get('/', (req, res) => {
     db.busker.findAll({
         include: [db.location, db.user]
     })
     .then(buskers => {
+        // mapboxgl.accessToken = 'pk.eyJ1Ijoibmlja3ViZWQiLCJhIjoiY2s0YWl3ZjJ6MDRnYTNrbzV3aTQ1bGlzcyJ9.BB2C_W2tJ5gK3Y_GhkBVSQ';
+        // var map = new mapboxgl.Map({
+        // container: 'map', // container id
+        // style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        // center: [-122.3321, 47.6062], // starting position [lng, lat]
+        // zoom: 9 // starting zoom
+        // });
         res.render('busker/index', { buskers })
     })
     .catch(err => {
@@ -49,6 +57,7 @@ router.post('/add', isLoggedIn, (req, res) => {
         rating: req.body.rating,
         userId: req.body.userId })
     db.location.create({
+        //Needs functionality to allow it to populate buskerLocation
         address: req.body.address,
         city: req.body.city,
         state: req.body.state,
