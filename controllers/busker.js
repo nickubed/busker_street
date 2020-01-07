1b38deb7719baf82d381b8ba77eaf53c63a00afd
nickubed <nhpquandt@gmail.com>
2020-01-07 13:24:38 -0800

    Added geocode functionality, database now properly populates

--- controllers/busker.js
+++ controllers/busker.js
@@ -34,6 +34,35 @@ router.get('/add', isLoggedIn, (req, res) => {
     })
 })
 
+/* ----------------------------- FROM BRANDON BLACK, MODIFIED ------------------------------- */
+
+router.get('/all', function(req, res) { // Shows all cities in our db
+    db.location.findAll()
+    .then(function(locations) {
+      let markers = locations.map(location => {
+        let markerObj = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [location.long, location.lat]
+          },
+          "properties": {
+            "title": location.address,
+            "icons": "../static/images/mapbox-icon.png"
+          }
+        }
+        return JSON.stringify(markerObj);
+      })
+      res.render('busker/allBuskTEMP', { locations, mapkey: process.env.MAPBOX_KEY, markers })
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error')
+    })
+  })
+
+/* ------------- END CONTENT FROM THE LEBRON JAMES OF MAPBOX CODE --------------------------- */
+
 router.get('/:id', (req, res) => {
     db.busker.findOne({
         where: {id: req.params.id}
@@ -47,42 +76,64 @@ router.get('/:id', (req, res) => {
     })
 })
 
+
+
 router.post('/add', isLoggedIn, (req, res) => {
-    let locations = {
-        address : req.body.address,
-        city : req.body.city,
-        state : req.body.state
-    }
-    db.busker.create({
-        name: req.body.name,
-        musicType: req.body.musicType,
-        description: req.body.description,
-        rating: req.body.rating,
-        userId: req.body.userId 
-    })
-        .then(busker => {
-            db.location.create({
-                address : locations.address,
-                city: locations.city,
-                state: locations.state
-            })
-            .then((location) => {
-                busker.addLocation(location)
+    //Pass location information into geocoder
+    geocode.forwardGeocode({
+        query: `${req.body.address}, ${req.body.city}, ${req.body.state}`,
+        types: ['place'],
+        countries: ['us']
+    }).send()
+        .then((response) => {
+            //responds with geocoded data, which is lat long
+            lat = response.body.features[0].center[1]
+            long = response.body.features[0].center[0]
+            //declaration of locations object
+        var locations = {
+            address : req.body.address,
+            city : req.body.city,
+            state : req.body.state,
+            lat: lat,
+            long: long
+        }
+        //begin population of db, here we do busker
+        db.busker.create({
+            name: req.body.name,
+            musicType: req.body.musicType,
+            description: req.body.description,
+            rating: req.body.rating,
+            userId: req.body.userId 
+        })
+        //populate location db
+            .then(busker => {
+                db.location.create({
+                    address : locations.address,
+                    city: locations.city,
+                    state: locations.state,
+                    lat: locations.lat,
+                    long: locations.long
                 })
+                //Adding location & busker to join table
+                .then((location) => {
+                    busker.addLocation(location)
+                    })
+                    .catch(generalError => {
+                        console.log(err)
+                    })
                 .catch(generalError => {
                     console.log(err)
                 })
-            .catch(generalError => {
-                console.log(err)
             })
+        })   
+        .then(function(busker) {
+            res.redirect('/busker')
         })
-    .then(function(busker) {
-        res.redirect('/busker')
+        .catch(err => {
+            console.log(err)
+            res.render('error')
+        })
     })
-    .catch(err => {
-        console.log(err)
-        res.render('error')
-    })
-})
+
 
 module.exports = router;
--- dbTest.js
+++ dbTest.js
@@ -2,73 +2,31 @@ var db = require('./models')
 // var async = require('async')
 // Create a category: Category model must exist and be migrated
 
-db.busker.create({
-  name: 'Smokey Dave',
-  musicType: 'Back-alley jazz',
-  description: `Dons many hats at once and has a different band every time I've seen him`,
-  rating: 3,
-  userId: 1
-}).then(function(busker) {
-  console.log(busker.get())
-})
-
-// Create a project and use the helper function create<ModelName> to create a category
-// Requires categoriesProjects to exist, be migrated, and properly associated
-
-// var cats = ['node', 'javascript', 'react', 'css', 'html']
-
-// db.project.create({
-//   name: 'PROJECT TWO',
-//   deployLink: 'http://github.com/brandiw',
-//   githubLink: 'http://github.com/brandiw',
-//   description: 'This was a game'
-// }).then(function(project) {
-//   // IMPROVED VERSION WITH ASYNC
-//   // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
-//   async.forEach(cats, (cat, done) => {
-//     db.category.findOrCreate({
-//       where: { name: cat }
-//     })
-//     .spread((category, wasCreated) => {
-//       project.addCategory(category)
-//       .then(() => {
-//         // res.redirect, or whatevs
-//         console.log('done adding', cat)
-//         done()
-//       })
-//     })
-//   }, () => {
-//     console.log('EVERYTHING is done. Now redirect or something')
-//   })
-
-
-  // db.project.findOne({
-  //   where: { id: 12 },
-  //   include: [db.category]
-  // }).then(function(project) {
-  //   // by using eager loading, the project model should have a categories key
-  //   console.log(project.categories)
-  
-    // a createCategory function should be available to this model
-    // project.createCategory({ name: 'node' }).then(function(category) {
-    //   console.log(category.get())
-    // })
-  
-
-
-
-  // TIMING DOESNOT WORK
-  // cats.forEach((cat) => {
-  //   db.category.findOrCreate({
-  //     where: { name: cat }
-  //   })
-  //   .spread((category, wasCreated) => {
-  //     project.addCategory(category)
-  //     .then(() => {
-  //       // res.redirect, or whatevs
-  //       console.log('done adding', cat)
-  //     })
-  //   })
-  // })
-  // console.log('redirect or something')
+// db.busker.create({
+//   name: 'Smokey Dave',
+//   musicType: 'Back-alley jazz',
+//   description: `Dons many hats at once and has a different band every time I've seen him`,
+//   rating: 3,
+//   userId: 1
+// }).then(function(busker) {
+//   console.log(busker.get())
 // })
+
+// router.get('/results', function (req, res) {
+  // TODO forward geocode using the req.query
+  geocode.forwardGeocode({
+    query: `${req.location.address}, ${req.location.city}, ${req.query.state}`,
+    types: ['place'],
+    countries: ['us']
+  }).send()
+  .then(function(response) {
+    let results = response.body.features.map(result => {
+      return {
+        name: result.place_name,
+        lat: result.center[1],
+        long: result.center[0]
+      }
+    })
+    res.render('cities/results', { query: req.query, results });
+  })
+// });
